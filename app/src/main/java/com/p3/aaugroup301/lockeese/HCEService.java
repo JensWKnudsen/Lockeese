a11481266bb7b4a79c1bf1d9338186dfa8d4c087
Lívia <livien.dz@gmail.com>
2019-10-16 14:41:00 +0200

    Changes in Android Manifest for NFC permissions New Clas HCEService which sends LockKey to NFC reader

---
+++ app/src/main/java/com/p3/aaugroup301/lockeese/HCEService.java
@@ -0,0 +1,22 @@
+package com.p3.aaugroup301.lockeese;
+
+import android.nfc.cardemulation.HostApduService;
+import android.os.Bundle;
+
+//sending LockKey to NFC reader
+public class HCEService extends HostApduService {
+    @Override
+    public byte[] processCommandApdu(byte[] bytes, Bundle bundle) {
+        // all communication should be encrypted
+       //a) get request from NFC reader to send LockKey -> send the LockKey
+        // b) LockKey is wrong -> mess 'the LockKey is incorrect'
+        // c) the was an error -> resend the LockKey
+
+        return new byte[0];
+    }
+
+    @Override
+    public void onDeactivated(int i) {
+
+    }
+}
