e76f236917be2bc18717518d94b10c07601f86a4
JensWKnudsen <jwkn17@student.aau.dk>
2019-12-14 10:30:08 +0100

    host card emulation

--- app/src/main/java/com/p3/aaugroup301/lockeese/HCEService.java
+++ app/src/main/java/com/p3/aaugroup301/lockeese/HCEService.java
@@ -4,18 +4,28 @@ import android.nfc.cardemulation.HostApduService;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.Arrays;
+
 //sending LockKey to NFC reader
 public class HCEService extends HostApduService {
+
+    String AID = "F2076423058596";
     @Override
     public byte[] processCommandApdu(byte[] bytes, Bundle bundle) {
 
-        Log.d("NFCTest", "message from reader: " + bytes);
+
+        if (Arrays.copyOfRange(bytes,10,24) == AID.getBytes())  {
+            return KeysListActivity.getListOfKeys().get(0).getKeyHash().getBytes();
+        } else {
+            return "Error".getBytes();
+        }
+
+
+        //Log.d("NFCTest", "message from reader: " + bytes);
         // all communication should be encrypted
        //a) get request from NFC reader to send LockKey -> send the LockKey
         // b) LockKey is wrong -> mess 'the LockKey is incorrect'
         // c) the was an error -> resend the LockKey
-
-        return new byte[0];
     }
 
     @Override
