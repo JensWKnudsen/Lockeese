9d003f714d02e1c5fdcf0ff5812cd4b11df16f0e
Ev <moturi.eve@gmail.com>
2020-01-22 09:34:39 +0100

    public key array 2 parts working

--- app/src/main/java/com/p3/aaugroup301/lockeese/HCEService.java
+++ app/src/main/java/com/p3/aaugroup301/lockeese/HCEService.java
@@ -4,6 +4,8 @@ import android.nfc.cardemulation.HostApduService;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.common.util.ArrayUtils;
+
 import java.nio.ByteBuffer;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
@@ -37,6 +39,9 @@ public class HCEService extends HostApduService {
     SecretKeySpec appAesKeySpec;
     KeysHashes usedKey;
     DBHandler DBHandler;
+    byte[] part1;
+    byte[] part2;
+
 
     @Override
     public byte[] processCommandApdu(byte[] bytes, Bundle bundle) {
@@ -47,21 +52,61 @@ public class HCEService extends HostApduService {
         Log.e("NFC","bytes[] has length: " + bytes.length);
         String AID = new String(Arrays.copyOfRange(bytes,0,12));
         Log.e("NFC","AID: " + AID);
+        Log.e("NFC","0. byte is " +new String(Arrays.copyOfRange(bytes,0,1)) );
+        Log.e("NFC","1. byte is " +new String(Arrays.copyOfRange(bytes,1,2)) );
+        Log.e("NFC","2. byte is " +new String(Arrays.copyOfRange(bytes,2,3)) );
+        Log.e("NFC","3. byte is " +new String(Arrays.copyOfRange(bytes,3,4)) );
+        Log.e("NFC","4. byte is " +new String(Arrays.copyOfRange(bytes,4,5)) );
+        Log.e("NFC","5. byte is " +new String(Arrays.copyOfRange(bytes,5,6)) );
+        Log.e("NFC","6. byte is " +new String(Arrays.copyOfRange(bytes,6,7)) );
+        Log.e("NFC","7. byte is " +new String(Arrays.copyOfRange(bytes,7,8)) );
+        Log.e("NFC","8. byte is " +new String(Arrays.copyOfRange(bytes,8,9)) );
+        Log.e("NFC","9. byte is " +new String(Arrays.copyOfRange(bytes,9,10)) );
+        Log.e("NFC","10. byte is " +new String(Arrays.copyOfRange(bytes,10,11)) );
+        Log.e("NFC","11. byte is " +new String(Arrays.copyOfRange(bytes,11,12)) );
+        Log.e("NFC","12. byte is " +new String(Arrays.copyOfRange(bytes,12,13)) );
+
         String stage = new String(Arrays.copyOfRange(bytes,12,13));
         Log.e("NFC","stage is: " + stage);
         String messagePart = new String(Arrays.copyOfRange(bytes,13,bytes.length));
         Log.e("NFC","extra info is: " + messagePart);
+      byte[] msgPartBytesArray = Arrays.copyOfRange(bytes,13,bytes.length);
+      Log.e("NFC", "size of the array :" + msgPartBytesArray.length);
+
+
 
 
         switch(stage)
         {
-            case "0" :
-                try {
+            case"0" :
+                Log.e("NFC", "case 0: ");
+                part1 = Arrays.copyOfRange(bytes,13,bytes.length);
 
+            return part1;
+
+            case "1" :
+                Log.e("NFC", "case 1: ");
+                part2 = Arrays.copyOfRange(bytes,13,bytes.length);
+
+               // byte[] finalArrayPublicKey = new byte[part1.length + part2.length];
+               byte[] finalArrayPublicKey = ArrayUtils.concatByteArrays(part1,part2);
+                Log.e("NFC", "size of the part1 array after concat is : " + part1.length);
+                Log.e("NFC", "size of the part2 array after concat is : " + part2.length);
+
+               Log.e("NFC", "size of the final array after concat is : " + finalArrayPublicKey.length);
+
+                Log.e("NFC", "before the try: ");
+                try {
+                    Log.e("NFC", "inside the try: ");
                     KeyFactory KeyFac = KeyFactory.getInstance("RSA");
-                    X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(Arrays.copyOfRange(bytes,13,bytes.length));
+                    Log.e("NFC", "AFTER KeyFactory.getInstance: ");
+                    //X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(Arrays.copyOfRange(bytes,13,bytes.length));
+                   // X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(msgPartBytesArray);
+                    X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(finalArrayPublicKey);
+                    Log.e("NFC", "after KeySpec " + x509KeySpec);
                     piAsymmetricPubKey = KeyFac.generatePublic(x509KeySpec);
 
+                    //Log.e("NFC", "if the key is not null: " + piAsymmetricPubKey);
                 } catch (NoSuchAlgorithmException e) {
                     e.printStackTrace();
                 } catch (InvalidKeySpecException e) {
@@ -70,7 +115,8 @@ public class HCEService extends HostApduService {
 
                 ArrayList<KeysHashes> keys = KeysListActivity.getListOfKeys();
                 for(KeysHashes key: keys){
-                    if (Arrays.equals(key.getPublicKey().getEncoded(),piAsymmetricPubKey.getEncoded())){
+                    if (Arrays.equals(key.getPublicKey().getEncoded(),
+                            piAsymmetricPubKey.getEncoded())){
                         usedKey = key;
                         return DBHandler.getPublicKey().getEncoded();
                     }
@@ -80,7 +126,7 @@ public class HCEService extends HostApduService {
                 return "stage 1 failed".getBytes();
 
 
-            case "1" :
+            case "2" :
                 byte[] encryptedDHPublicKeyOfApp = new byte[0];
 
                 try {
@@ -125,7 +171,7 @@ public class HCEService extends HostApduService {
                 return encryptedDHPublicKeyOfApp;
 
 
-            case "2" :
+            case "3" :
                 try {
                     byte[] ivLentgh = Arrays.copyOfRange(Arrays.copyOfRange(bytes,13,bytes.length),0,8);
 
